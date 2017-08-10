c36769d40cf1718e1e7ad3113a890a25be0e05eb
Vasiliy Shubko <flit77@gmail.com>
2017-08-10 23:28:04 +0200

    component moved to separate file

--- src/main.js
+++ src/main.js
@@ -1,33 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import noteReducer from './reducers/note';
+import Notes from './components/Notes.jsx';
 
 const CREATE_NOTE = 'CREATE_NOTE';
 const UPDATE_NOTE = 'UPDATE_NOTE';
 
-const onAddNote = () => {
-  const id = window.state.nextNoteId;
-  window.state.notes[id] = {
-    id,
-    content: ''
-  };
-  window.state.nextNoteId++;
-  renderApp();
+const initialState = {
+  nextNoteId: 1,
+  notes: {}
 };
 
-const NoteApp = ({ notes }) =>
-  <div>
-    <ul className="note-list">
-      {Object.keys(notes).map(id =>
-        <li className="note-list-item" key={id}>
-          {id}
-        </li>
-      )}
-    </ul>
-    <button className="editor-button" onClick={onAddNote}>
-      New Note
-    </button>
-  </div>;
+window.state = initialState;
 
 const state0 = noteReducer(undefined, {
   type: CREATE_NOTE
@@ -41,10 +25,10 @@ const state1 = noteReducer(state0, {
 
 const renderApp = () => {
   ReactDOM.render(
-    // <NoteApp notes={window.state.notes} />,
-    <pre>
-      {JSON.stringify(state1, null, 2)}
-    </pre>,
+    <Notes notes={window.state.notes} />,
+    // <pre>
+    //   {JSON.stringify(state1, null, 2)}
+    // </pre>,
     document.getElementById('root')
   );
 };
