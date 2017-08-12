365e9b432f527fc5257032dbb1be02af2caeae24
Vasiliy Shubko <flit77@gmail.com>
2017-08-12 12:51:15 +0200

    provider added

---
+++ src/provider.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Provider extends React.Component {
+  getChildContext() {
+    return {
+      store: this.props.store
+    };
+  }
+  render() {
+    return this.props.children;
+  }
+}
+
+Provider.childContextTypes = {
+  store: PropTypes.object
+};
+
+export default Provider;
